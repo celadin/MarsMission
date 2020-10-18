c0f3aec03cf5fc39432418e85d1fc2612c9ef92c
Celalettin Altıntaş <CELALETTIN ALTINTA>
2020-10-18 14:43:11 +0300

    Solution and Projects Added

---
+++ MarsMission/MarsMission.UI.CmdConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarsMission.UI.CmdConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
